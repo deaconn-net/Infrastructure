c71c6d9316c191e66d4b1bded6690bc330071e28
Christian Deacon <christian@moddingcommunity.com>
2022-09-14 23:44:02 -0500

    Add startup.sh file executed with Docker Compose.

---
+++ start.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Import SSH key.
+function run_cmd()
+{
+        ssh-agent bash -c "GIT_SSH_COMMAND='ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no' $1"
+}
+
+
+# Check if we need to clone.
+if [ ! -d "back-bone" ] ; then
+        run_cmd "git clone git@github.com:Deaconn-net/back-bone.git"
+fi
+
+# Pull from remote.
+cd back-bone/
+git config pull.rebase false
+run_cmd "git pull origin master"
+
+# Make sure background video exists.
+#cp -n ../bg.mp4 deaconn/home/static/home/bg.mp4
+
+# Collect files.
+cd deaconn
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput --clear
+
+# Start server.
+gunicorn --bind unix:/deaconn/data/srv.sock deaconn.wsgi
